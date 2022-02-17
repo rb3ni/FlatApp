30f3ac687346de9be452b063603b939b9c47d7c1
rb3ni <regeisz.beni@gmail.com>
2022-02-17 23:00:31 +0100

    EventTableRepository createNewEventConnectionTable(Event event) method done with addbatch

--- src/main/java/hu/repository/EventTableRepository.java
+++ src/main/java/hu/repository/EventTableRepository.java
@@ -1,9 +1,13 @@
 package hu.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import hu.domain.account.Account;
+import hu.domain.event.Complain;
+import hu.domain.event.Emergency;
+import hu.domain.event.Event;
+import hu.domain.event.Reminder;
+import hu.domain.space.Space;
+
+import java.sql.*;
 
 import static hu.repository.DatabaseConfigFlatApp.*;
 
@@ -19,9 +23,9 @@ public class EventTableRepository {
         }
     }
 
-    public void createEventTable() {
+    public void createEventConnectionTable() {
         String sqlCreateTable = "CREATE TABLE IF NOT EXISTS event_table (" +
-                "id INT PRIMARY KEY, " +
+                "id INT PRIMARY KEY AUTO_INCREMENT, " +
                 "space_id INT, " +
                 "account_id INT, " +
                 "event_id INT, " +
@@ -35,6 +39,36 @@ public class EventTableRepository {
         }
     }
 
+    public String createNewEventConnectionTable(Event event) {
+        String infoBack = "Event_table can not be created";
+        String insertEventStatement = "INSERT INTO event_table VALUES (?,?,?,?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertEventStatement)) {
 
-
+            if (event instanceof Complain) {
+                for (Account receiver : ((Complain) event).getReceivers()) {
+                    preparedStatement.setInt(4, event.getId());
+                    preparedStatement.setInt(3, receiver.getId());
+                    preparedStatement.addBatch();
+                }
+            } else if (event instanceof Emergency) {
+                for (Space space : ((Emergency) event).getAffectedSpaces()) {
+                    preparedStatement.setInt(4, event.getId());
+                    preparedStatement.setInt(3, space.getId());
+                    preparedStatement.addBatch();
+                }
+            } else {
+                for (Space space : ((Reminder) event).getAffectedSpaces()) {
+                    preparedStatement.setInt(4, event.getId());
+                    preparedStatement.setInt(3, space.getId());
+                    preparedStatement.addBatch();
+                }
+            }
+            preparedStatement.executeBatch();
+            infoBack = "Event_table created";
+        } catch (
+                SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return infoBack;
+    }
 }
