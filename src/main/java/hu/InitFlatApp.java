df446ac1b75c27e852dc452a335bfdc076c72ead
Bohár Dávid <bohardawid@gmail.com>
2022-02-18 00:54:20 +0100

    Transactions, Space módosítások, block bővítés, Init letisztázása ha az InitFlatApp-et futtatod, feltölti az adatbázist pár adattal, amivel már lehet tesztelgetni az eddigieket

--- src/main/java/hu/InitFlatApp.java
+++ src/main/java/hu/InitFlatApp.java
@@ -1,33 +1,49 @@
 package hu;
 
+import hu.domain.Block;
 import hu.domain.account.ExternalService;
 import hu.domain.account.Habitant;
-import hu.domain.space.SpaceMod;
+import hu.domain.space.Space;
 import hu.domain.space.SpaceType;
 import hu.repository.*;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class InitFlatApp {
+
     public static void main(String[] args) {
         TransactionRepository transactionRepository = new TransactionRepository();
         AccountRepository accountRepository = new AccountRepository();
         SpaceRepository spaceRepository = new SpaceRepository();
         SpaceTypeRepository spaceTypeRepository = new SpaceTypeRepository();
-        SpaceRepositoryMod spaceRepositoryMod = new SpaceRepositoryMod();
         PropertyTable propertyTable = new PropertyTable();
+        BlockRepository blockRepository = new BlockRepository();
 
         accountRepository.createAccountTable();
         transactionRepository.createTransactionTable();
         spaceTypeRepository.createSpaceTypeRepository();
-        spaceRepositoryMod.createSpaceTable();
+        spaceRepository.createSpaceTable();
         propertyTable.createPropertyTable();
+        blockRepository.createBlockTable();
 
 
         Habitant habitant;
         ExternalService externalService;
         SpaceType spaceType;
-        SpaceMod space;
+        SpaceType spaceType1;
+        SpaceType spaceType2;
+        SpaceType spaceType3;
+        SpaceType spaceType4;
+        SpaceType spaceType5;
+        Space space;
+        Space space2;
+        Space space3;
+        Space space4;
+        Space space5;
+        Block block;
 
         List<Integer> emptyIdList = null;
 
@@ -49,14 +65,14 @@ public class InitFlatApp {
                 "Escobar&Escobar kft.");
 
 
-        //System.out.println(accountRepository.createNewAccount(habitant));
-        //System.out.println(accountRepository.createNewAccount(externalService));
-        //System.out.println(accountRepository.searchAccountById(1));
-        //System.out.println(accountRepository.searchAccountById(2));
+        System.out.println(accountRepository.createNewAccount(habitant));
+        System.out.println(accountRepository.createNewAccount(externalService));
+        System.out.println(accountRepository.searchAccountById(1));
+        System.out.println(accountRepository.searchAccountById(2));
         System.out.println(accountRepository.accountIdList());
-        //accountRepository.overwriteAccountIdByName("Bruce Wayne",111111);
+        accountRepository.overwriteAccountIdByName("Bruce Wayne", 111111);
 
-        spaceType = new SpaceType("SpaceTypeA",
+        spaceType = new SpaceType("spaceTypeA",
                 2,
                 53,
                 true,
@@ -64,15 +80,80 @@ public class InitFlatApp {
                 "Két szobás, erkéllyel rendelkező lakás, belső elhelyezkedésű.",
                 1);
 
-        space = new SpaceMod(1, 6, null, "SpaceTypeA", 1);
+        spaceType2 = new SpaceType("spaceTypeB",
+                2,
+                51,
+                false,
+                30000,
+                "Két szobás lakás, belső elhelyezkedésű.",
+                1);
 
-        //spaceTypeRepository.createNewSpaceType(spaceType);
-        //spaceRepositoryMod.createNewSpace(space);
+        spaceType3 = new SpaceType("spaceTypeC",
+                1.5,
+                45,
+                false,
+                30000,
+                "Másfél szobás lakás, külső elhelyezkedésű.",
+                1);
 
-        //transactionRepository.readTransactions("src/main/resources/Transactions22_02.csv");
+        spaceType4 = new SpaceType("parkingSlot",
+                0,
+                6,
+                false,
+                10000,
+                "Parkolóhely",
+                1);
 
-        propertyTable.assignHabitantAndSpace(11111,2);
+        spaceType5 = new SpaceType("businessSpace",
+                4,
+                96,
+                false,
+                60000,
+                "Földszinti üzlethelyiség",
+                1);
+
+        space = new Space(1, 7, null, "spaceTypeA", 1, 0);
+        space2 = new Space(1, 8, null, "spaceTypeB", 1, 0);
+        space3 = new Space(1, 9, null, "spaceTypeC", 1, 0);
+        space4 = new Space(-1, 0, null, "parkingSlot", 1, 0);
+        space5 = new Space(0, 1, null, "businessSpace", 1, 0);
+
+        System.out.println(spaceTypeRepository.createNewSpaceType(spaceType));
+        System.out.println(spaceTypeRepository.createNewSpaceType(spaceType2));
+        System.out.println(spaceTypeRepository.createNewSpaceType(spaceType3));
+        System.out.println(spaceTypeRepository.createNewSpaceType(spaceType4));
+        System.out.println(spaceTypeRepository.createNewSpaceType(spaceType5));
+        spaceRepository.createNewSpace(space);
+        spaceRepository.createNewSpace(space2);
+        spaceRepository.createNewSpace(space3);
+        spaceRepository.createNewSpace(space4);
+        spaceRepository.createNewSpace(space5);
+
+
+        String deadLineS = "2022-01-15";
+        String startingDateS = "2022-01-01";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate deadlineLocal = LocalDate.parse(deadLineS, formatter);
+        LocalDate startingDateLocal = LocalDate.parse(startingDateS, formatter);
+        Date deadline = java.sql.Date.valueOf(deadlineLocal);
+        Date startingDate = java.sql.Date.valueOf(startingDateLocal);
+
+        block = new Block(1, "Macondo", 4321, "Fő út", 1, "", 40, 5, null, null, deadline, startingDate);
+
+        blockRepository.createNewBlock(block);
+
+        transactionRepository.readTransactions("src/main/resources/Transactions22_02.csv");
+        System.out.println(transactionRepository.unassignedTransactions());
+
+        propertyTable.assignHabitantAndSpace(111111, 1);
+        propertyTable.assignHabitantAndSpace(111111, 2);
+        System.out.println(propertyTable.searchSpacesByHabitantId(111111));
+
+        block.updateTransactions();
 
     }
 
+
 }
+
+
--- src/main/java/hu/domain/Block.java
+++ src/main/java/hu/domain/Block.java
@@ -1,12 +1,22 @@
 package hu.domain;
 
-import hu.domain.account.Account;
-import hu.domain.space.Space;
+import hu.repository.TransactionRepository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class Block {
+
+    TransactionRepository transactionRepository = new TransactionRepository();
 
     private int id;
     private String city;
@@ -19,10 +29,23 @@ public class Block {
     private List<Integer> spaces;
     private List<Integer> accounts;
     private Date paymentDeadline;
+    private Date startingDate;
 
-    public Block(int id, String city, int postalCode, String street, int houseNumber, String description,
-                 int numberOfFlats, int numberOfFloors, List<Integer> spaces, List<Integer> accounts,
-                 Date paymentDeadline) {
+    public Block(String city, int postalCode, String street, int houseNumber, String description, int numberOfFlats, int numberOfFloors, List<Integer> spaces, List<Integer> accounts, Date paymentDeadline, Date startingDate) {
+        this.city = city;
+        this.postalCode = postalCode;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.description = description;
+        this.numberOfFlats = numberOfFlats;
+        this.numberOfFloors = numberOfFloors;
+        this.spaces = spaces;
+        this.accounts = accounts;
+        this.paymentDeadline = paymentDeadline;
+        this.startingDate = startingDate;
+    }
+
+    public Block(int id, String city, int postalCode, String street, int houseNumber, String description, int numberOfFlats, int numberOfFloors, List<Integer> spaces, List<Integer> accounts, Date paymentDeadline, Date startingDate) {
         this.id = id;
         this.city = city;
         this.postalCode = postalCode;
@@ -34,7 +57,53 @@ public class Block {
         this.spaces = spaces;
         this.accounts = accounts;
         this.paymentDeadline = paymentDeadline;
+        this.startingDate = startingDate;
     }
+
+
+    public void updateTransactions() {
+        transactionRepository.readTransactions("src/main/resources/Transactions22_02.csv");
+
+        java.sql.Date previousDeadline = transactionRepository.getDeadline();
+        java.sql.Date currentDate;
+        LocalDate localDate = LocalDate.now();
+        currentDate = java.sql.Date.valueOf(localDate);
+        java.sql.Date updatedDate = deadlineChecker(currentDate);
+
+        if (!currentDate.equals(updatedDate)){
+            System.out.println(previousDeadline);
+            System.out.println(updatedDate);
+
+            //TODO balance frissítés
+            //TODO reminderek küldése
+        }
+
+
+    }
+
+
+    private java.sql.Date deadlineChecker(java.sql.Date currentDate) {
+
+            java.sql.Date deadline = transactionRepository.getDeadline();
+
+            if (deadline.compareTo(currentDate) < 0) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(deadline);
+                calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH) + 1);
+                java.util.Date newDeadline = calendar.getTime();
+
+                SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                String formattedDate = format1.format(newDeadline);
+                LocalDate newDeadlineLocal = LocalDate.parse(formattedDate, formatter);
+                deadline = java.sql.Date.valueOf(newDeadlineLocal);
+            }
+
+
+        return deadline;
+    }
+
 
     public int getId() {
         return id;
@@ -122,5 +191,9 @@ public class Block {
 
     public void setPaymentDeadline(Date paymentDeadline) {
         this.paymentDeadline = paymentDeadline;
+    }
+
+    public Date getStartingDate() {
+        return startingDate;
     }
 }
